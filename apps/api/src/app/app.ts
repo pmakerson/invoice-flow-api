cc72e858f6f3f50b8b67b7e4d0332746e87e7559
pmakerson <pierremakerson@gmail.com>
2026-04-09 19:49:25 +0200

    chore : add technical route

--- apps/api/src/app/app.ts
+++ apps/api/src/app/app.ts
@@ -1,20 +1,23 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { halfvec } from 'drizzle-orm/pg-core';
 import { registerErrorHandler } from '../shared/errors/error-handler.js';
 import { registerSwagger } from '../config/swagger.js';
+import { healthRoutes } from '../modules/health/health.routes.js';
 
 export async function buildApp() {
     const app = Fastify({
         logger: true
     });
+
+    registerErrorHandler(app);
+
     await app.register(cors, {
         origin: true
     });
 
     await registerSwagger(app);
 
-    registerErrorHandler(app);
+    await healthRoutes(app);
 
     return app;
 }
