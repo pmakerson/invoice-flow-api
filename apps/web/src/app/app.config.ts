e4628badd94e0980a7d734d51bae87bbe1b6200f
pmakerson <pierremakerson@gmail.com>
2026-04-11 18:32:29 +0200

    chore : init angular project

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,14 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient()
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
