445a99d7a84d03a02044bfb19fdefaf10992c0f2
pmakerson <pierremakerson@gmail.com>
2026-04-11 21:46:31 +0200

    feature : create page invoice

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { InvoicesPage } from './features/invoices/pages/invoices-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'invoices'
+},
+{
+    path: 'invoices',
+    component: InvoicesPage
+}];
