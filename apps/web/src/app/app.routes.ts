07f26a7eba89949cf0d1bb5bd4ff4655b96c840d
pmakerson <pierremakerson@gmail.com>
2026-04-11 18:32:29 +0200

    chore : init angular project

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { InvoicesPage } from './features/invoices/pages/invoices-page';
 
-export const routes: Routes = [{
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'invoices'
-},
-{
-    path: 'invoices',
-    component: InvoicesPage
-}];
+export const routes: Routes = [];
