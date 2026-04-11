671790fc8c2b2a917865304d53bcca03e921502d
pmakerson <pierremakerson@gmail.com>
2026-04-11 21:46:31 +0200

    feature : create page invoice

---
+++ apps/web/src/app/features/invoices/pages/invoices-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoices-page',
+  imports: [],
+  templateUrl: './invoices-page.html',
+  styleUrl: './invoices-page.css',
+})
+export class InvoicesPage {}
