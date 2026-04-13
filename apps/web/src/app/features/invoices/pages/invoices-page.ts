f82c6142f63a6c65945bbcdeba4fd8d08c4f2f1b
pmakerson <pierremakerson@gmail.com>
2026-04-13 16:33:31 +0200

    feature : add loading indicator

--- apps/web/src/app/features/invoices/pages/invoices-page.ts
+++ apps/web/src/app/features/invoices/pages/invoices-page.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core';
 import { InvoiceTableComponent } from '../ui/invoice-table/invoice-table.component';
 import { InvoiceStore } from '../data-access/invoice.store';
 import { InvoiceHistoryComponent } from '../ui/invoice-history/invoice-history.component';
+import { LoadingIndicatorComponent } from '../ui/loading-indicator/loading-indicator.component';
 
 @Component({
   selector: 'app-invoices-page',
-  imports: [InvoiceTableComponent, InvoiceHistoryComponent],
+  imports: [InvoiceTableComponent, InvoiceHistoryComponent, LoadingIndicatorComponent],
   templateUrl: './invoices-page.html',
   styleUrl: './invoices-page.css',
 })
